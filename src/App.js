a7aec534fd1cc5aba83e14d8cd7bbc7b05da7382
Diego Fontalvo <d.andres2002@hotmail.com>
2022-10-17 14:25:01 -0500

    Agregado componente lista de usuarios

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import { createContext } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { UserForm } from './components/UserForm';
-
-export const UsersContext = createContext();
+import { UsersList } from './components/UsersList';
+import { getUsers } from './services/users.service';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+  const [loadingUsers, setLoadingUsers] = useState(false)
+
+  const loadUsers = async () => {
+    setLoadingUsers(true);
+    const users = await getUsers();
+    setLoadingUsers(false);
+    setUsers(users);
+  }
+
+  useEffect(() => { loadUsers() }, [])
+
   return (
     <div className="container">
       <div className='row'>
         <div className='col-12 text-center my-3'><h1>GESTION DE USUARIOS</h1></div>
         <div className='col-12 col-lg-3'>
-          <UserForm />
+          <UserForm onSave={() => loadUsers()} />
+        </div>
+        <div className='col-12 col-lg-9'>
+          <UsersList users={users} loading={loadingUsers} />
         </div>
       </div>
     </div>
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore/lite';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCobETVP2VCZzGaX0CeUm7x7UaSg9WTlDk",
-  authDomain: "react-crud-df.firebaseapp.com",
-  projectId: "react-crud-df",
-  storageBucket: "react-crud-df.appspot.com",
-  messagingSenderId: "908603827245",
-  appId: "1:908603827245:web:4da878da11cc74dda284a4"
+    apiKey: "AIzaSyCobETVP2VCZzGaX0CeUm7x7UaSg9WTlDk",
+    authDomain: "react-crud-df.firebaseapp.com",
+    projectId: "react-crud-df",
+    storageBucket: "react-crud-df.appspot.com",
+    messagingSenderId: "908603827245",
+    appId: "1:908603827245:web:4da878da11cc74dda284a4"
 };
 
 const app = initializeApp(firebaseConfig);
