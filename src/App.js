b36308d6e0b3082827f462c84f149a1a05d4fca1
Diego Fontalvo <d.andres2002@hotmail.com>
2022-10-17 13:36:19 -0500

    Agregado componente de formulario de usuarios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { createContext } from 'react';
 import './App.css';
+import { UserForm } from './components/UserForm';
+
+export const UsersContext = createContext();
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
+      <div className='row'>
+        <div className='col-12 text-center my-3'><h1>GESTION DE USUARIOS</h1></div>
+        <div className='col-12 col-lg-3'>
+          <UserForm />
+        </div>
+      </div>
     </div>
   );
 }
