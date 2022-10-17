e37d90772ff72c2e506752ee34abc93f57bf1529
Diego Fontalvo <d.andres2002@hotmail.com>
2022-10-17 15:31:04 -0500

    Bloqueo de formulario al guardar usuario

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -14,6 +14,7 @@ export const UserForm = ({ onSave, onCancel, user, editing }) => {
   const [gender, setGender] = useState('');
   const [job, setJob] = useState('');
 
+  const [saving, setSaving] = useState(false)
   const [validationMessages, setValidationMessages] = useState({})
 
   useEffect(() => {
@@ -28,12 +29,9 @@ export const UserForm = ({ onSave, onCancel, user, editing }) => {
   }, [user])
 
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    if (validData()) {
-      await saveUser();
-      resetFields();
-    }
+    if (validData()) saveUser()
   }
 
   const resetFields = () => {
@@ -48,7 +46,10 @@ export const UserForm = ({ onSave, onCancel, user, editing }) => {
 
   const saveUser = async () => {
     const userData = { firstname, lastname, phone, address, birthday, gender, job }
+    setSaving(true)
     editing ? await updateUser(user.id, userData) : await addUser(userData)
+    resetFields()
+    setSaving(false)
     if (onSave) onSave()
   }
 
@@ -136,27 +137,27 @@ export const UserForm = ({ onSave, onCancel, user, editing }) => {
         <div className="card-body">
           <div className="mb-3">
             <label className="form-label">Nombres</label>
-            <input className={`form-control form-control-sm ${validationMessages.name ? 'is-invalid' : ''}`} type="text" min={0} value={firstname} onChange={e => setFirstname(e.target.value)} />
+            <input className={`form-control form-control-sm ${validationMessages.name ? 'is-invalid' : ''}`} disabled={saving} type="text" min={0} value={firstname} onChange={e => setFirstname(e.target.value)} />
             <div className="invalid-feedback d-block">{validationMessages.name ?? ''}</div>
           </div>
           <div className="mb-3">
             <label className="form-label">Apellidos</label>
-            <input className={`form-control form-control-sm ${validationMessages.lastname ? 'is-invalid' : ''}`} type="text" min={0} value={lastname} onChange={e => setLastname(e.target.value)} />
+            <input className={`form-control form-control-sm ${validationMessages.lastname ? 'is-invalid' : ''}`} disabled={saving} type="text" min={0} value={lastname} onChange={e => setLastname(e.target.value)} />
             <div className="invalid-feedback d-block">{validationMessages.lastname ?? ''}</div>
           </div>
           <div className="mb-3">
             <label className="form-label">Dirección</label>
-            <input className={`form-control form-control-sm ${validationMessages.address ? 'is-invalid' : ''}`} type="text" min={0} value={address} onChange={e => setAdress(e.target.value)} />
+            <input className={`form-control form-control-sm ${validationMessages.address ? 'is-invalid' : ''}`} disabled={saving} type="text" min={0} value={address} onChange={e => setAdress(e.target.value)} />
             <div className="invalid-feedback d-block">{validationMessages.address ?? ''}</div>
           </div>
           <div className="mb-3">
             <label className="form-label">Telefono</label>
-            <input className={`form-control form-control-sm ${validationMessages.phone ? 'is-invalid' : ''}`} type="number" min={0} value={phone} onChange={e => setPhone(e.target.value)} />
+            <input className={`form-control form-control-sm ${validationMessages.phone ? 'is-invalid' : ''}`} disabled={saving} type="number" min={0} value={phone} onChange={e => setPhone(e.target.value)} />
             <div className="invalid-feedback d-block">{validationMessages.phone ?? ''}</div>
           </div>
           <div className="mb-3">
             <label className="form-label">Genero</label>
-            <select className={`form-select form-select-sm ${validationMessages.gender ? 'is-invalid' : ''}`} value={gender} onChange={e => setGender(e.target.value)}>
+            <select className={`form-select form-select-sm ${validationMessages.gender ? 'is-invalid' : ''}`} disabled={saving} value={gender} onChange={e => setGender(e.target.value)}>
               <option selected hidden>-</option>
               {genderList.map(gender => <option>{gender}</option>)}
             </select>
@@ -164,17 +165,17 @@ export const UserForm = ({ onSave, onCancel, user, editing }) => {
           </div>
           <div className="mb-3">
             <label className="form-label">Cargo</label>
-            <input className={`form-control form-control-sm ${validationMessages.job ? 'is-invalid' : ''}`} value={job} onChange={e => setJob(e.target.value)} />
+            <input className={`form-control form-control-sm ${validationMessages.job ? 'is-invalid' : ''}`} disabled={saving} value={job} onChange={e => setJob(e.target.value)} />
             <div className="invalid-feedback d-block">{validationMessages.job ?? ''}</div>
           </div>
           <div className="mb-3">
             <label className="form-label">Fecha de Nacimiento</label>
-            <input className={`form-control form-control-sm ${validationMessages.birthday ? 'is-invalid' : ''}`} type="date" value={birthday} onChange={e => setBirthday(e.target.value)} />
+            <input className={`form-control form-control-sm ${validationMessages.birthday ? 'is-invalid' : ''}`} disabled={saving} type="date" value={birthday} onChange={e => setBirthday(e.target.value)} />
             <div className="invalid-feedback d-block">{validationMessages.birthday ?? ''}</div>
           </div>
           <div className='d-flex justify-content-center'>
-            <input type='submit' className={`btn btn-${editing ? 'warning' : 'primary'} w-50`} value={editing ? 'Actualizar' : 'Agregar'} />
-            {editing && <button type='button' className='btn btn-danger w-50' onClick={() => onCancel()}>Cancelar</button>}
+            <input type='submit' className={`btn btn-${editing ? 'warning' : 'primary'} w-50`} disabled={saving} value={editing ? 'Actualizar' : 'Agregar'} />
+            {editing && <button type='button' className='btn btn-danger w-50' disabled={saving} onClick={() => onCancel()}>Cancelar</button>}
           </div>
         </div>
       </div>
